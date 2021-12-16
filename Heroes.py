cdec4672a0c4654662352ac9ca2c549538b30a36
Yanis Zeghiche <73747639+Sprk3lzZ@users.noreply.github.com>
2021-12-16 16:09:14 +0100

    next uploads

--- Heroes.py
+++ Heroes.py
@@ -1,2 +1,30 @@
 class Barbare:
-    __init__(name="Conan", initiative=12, atk=7,  )
+
+    def __init__(self, name="Conan", initiative=12, dmg=7, pv=120):
+        self.name = name
+        self.initiative = initiative
+        self.dmg = dmg
+        self.pv = pv
+
+    def get_name(self):
+        return self.name
+
+    def get_initiative(self):
+        return self.initiative
+
+    def get_dmg(self):
+        return self.dmg
+
+    def get_pv(self):
+        return self.pv
+
+    def __str__(self):
+        return f"{self.name} le Barbare ({self.pv} PV)"
+
+    def decrement_PV(self, damage):
+        self.pv -= damage
+
+
+combatant = Barbare("YANIS")
+
+print(combatant)
--- Hostiles.py
+++ Hostiles.py
@@ -1,1 +1,35 @@
-print("Hostiles")
+from random import randint, random
+
+
+class Montre:
+    _ListeArme = ["couteau", "sabre", "marteau"]
+
+    _PuissanceArme = {"couteau": 3, "sabre": 6, "marteau": 9}
+
+    def __init__(self, arme='thomson', max_dmg=10, initiative=randint(5, 20), pv=6):
+        self.arme = arme
+        self.max_dmg = max_dmg
+        self.initiative = initiative
+        self.pv = pv
+        self.arme = self._ListeArme[randint(0,2)]
+        self.max_dmg = self._PuissanceArme[self.arme]
+
+    def __str__(self):
+        return f"Un monstre armé d'un {self.arme} avec une initiative de {self.initiative}"
+
+    def get_arme(self):
+        return self.arme
+
+    def get_max_dmg(self):
+        return self.max_dmg
+
+    def get_initiative(self):
+        return self.initiative
+
+    def get_pv(self):
+        return self.pv
+
+
+monstre = Montre()
+
+print(monstre)
